c55e411be3e0988f2bc3c1d3d1b06dfcf930777f
Rafael Laidlaw <rafalaidlaw@gmail.com>
2025-01-28 14:42:18 -0500

    Refactored code for better readability

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,31 +1,125 @@
 import Phaser from "phaser";
 
+const PIPES_TO_RENDER = 4;
+
 class PlayScene extends Phaser.Scene {
   constructor(config) {
     super("PlayScene");
     this.config = config;
 
-    this.kilroy = null;
+    this.kilboy = null;
+    this.pipes = null;
+
+    this.pipeHorizontalDistance = 0;
+    this.pipeVerticalDistanceRange = [150, 250];
+    this.pipeHorizontalDistanceRange = [450, 500];
+    this.flapVELOCITY = 300;
   }
 
   preload() {
     this.load.image("sky-bg", "assets/sky.png");
-    this.load.image("kilroy", "assets/kilroy.png");
+    this.load.image("kilboy", "assets/kilboy.png");
+    this.load.image("pipe", "assets/pipe.png");
   }
 
   create() {
+    this.createBG();
+    this.createKilboy();
+    this.createPipes();
+    this.handleInputs();
+  }
+
+  update() {
+    this.checkGameStatus();
+
+    this.recyclePipes();
+  }
+
+  createBG() {
     this.add.image(0, 0, "sky-bg").setOrigin(0);
-    this.kilroy = this.physics.add
+  }
+  createKilboy() {
+    this.kilboy = this.physics.add
       .sprite(
         this.config.startPosition.x,
         this.config.startPosition.y,
-        "kilroy"
+        "kilboy"
       )
       .setOrigin(0);
-    this.kilroy.body.gravity.y = 400;
+    this.kilboy.body.gravity.y = 400;
+  }
+  createPipes() {
+    this.pipes = this.physics.add.group();
+    for (let i = 0; i < PIPES_TO_RENDER; i++) {
+      const upperPipe = this.pipes.create(0, 0, "pipe").setOrigin(0, 1);
+      const lowerPipe = this.pipes.create(0, 0, "pipe").setOrigin(0);
+
+      this.placePipe(upperPipe, lowerPipe);
+    }
+
+    this.pipes.setVelocityX(-200);
+  }
+  handleInputs() {
+    this.input.on("pointerdown", this.flap, this);
+
+    this.input.keyboard.on("keydown-SPACE", this.flap, this);
+  }
+  checkGameStatus() {
+    if (
+      this.kilboy.y <= 0 - this.kilboy.height ||
+      this.kilboy.y >= this.config.height
+    ) {
+      this.restartkilboyPosition();
+    }
+  }
+  placePipe(upPipe, lowPipe) {
+    const rightMostX = this.getRightMostPipe();
+    const pipeVerticalDistance = Phaser.Math.Between(
+      ...this.pipeVerticalDistanceRange
+    );
+    const pipeVerticalPosition = Phaser.Math.Between(
+      0 + 20,
+      this.config.height - 20 - pipeVerticalDistance
+    );
+    const pipeHorizontalDistance = Phaser.Math.Between(
+      ...this.pipeHorizontalDistanceRange
+    );
+
+    upPipe.x = rightMostX + pipeHorizontalDistance;
+    upPipe.y = pipeVerticalPosition;
+
+    lowPipe.x = upPipe.x;
+    lowPipe.y = upPipe.y + pipeVerticalDistance;
+  }
+  recyclePipes() {
+    const tempPipes = [];
+    this.pipes.getChildren().forEach((pipe) => {
+      if (pipe.getBounds().right <= -1) {
+        tempPipes.push(pipe);
+        if (tempPipes.length === 2) {
+          this.placePipe(...tempPipes);
+        }
+      }
+    });
+  }
+  getRightMostPipe() {
+    let rightMostX = 0;
+    this.pipes.getChildren().forEach(function (pipe) {
+      rightMostX = Math.max(pipe.x, rightMostX);
+    });
+    return rightMostX;
   }
 
-  update() {}
+  restartkilboyPosition() {
+    this.kilboy.x = this.config.startPosition.x;
+    this.kilboy.y = this.config.startPosition.y;
+    this.kilboy.body.velocity.y = 0;
+    this.kilboy.gravity = 0;
+  }
+
+  flap() {
+    this.kilboy.body.velocity.y = -this.flapVELOCITY;
+  }
 }
 
 export default PlayScene;
